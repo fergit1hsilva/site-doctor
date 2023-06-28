b92d6e144f5ea18052ae10cd0a926a37c24d74cf
fernandohsilva1 <fernandohsilva1@hotmail.com>
2023-06-28 11:00:06 -0300

    pj

---
+++ assets/js/google-maps.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var myLatLng = {lat: -6.207690, lng: 106.985270};
+
+    var map = new google.maps.Map(document.getElementById('google-maps'), {
+        zoom: 17,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'South Jakarta, INA'
+    });
+}
---
+++ assets/js/theme.js
@@ -0,0 +1,62 @@
+
+$('document').ready(function() {
+  $('#doctorSlideshow').owlCarousel({
+    nav: true,
+    dots: false,
+    navText: ["<span class='mai-arrow-back'></span>", "<span class='mai-arrow-forward'></span>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+});
+
+$('document').ready(function() {
+  $("a[data-role='smoothscroll']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top - nav_height;
+
+    $("body, html").animate({
+        scrollTop: position
+    }, 1000 );
+    return false;
+  });
+});
+
+$('document').ready(function() {
+  var backTop = $(".back-to-top");
+
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 400) {
+      backTop.css('visibility', 'visible');
+    }
+    else if($(document).scrollTop() < 400) {
+      backTop.css('visibility', 'hidden');
+    }
+  });
+
+  backTop.click(function() {
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+});
+
+
+$('document').ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('[data-toggle="popover"]').popover();
+
+  new WOW().init();
+});
+
